082d26e0cffcd863ddb8d663918dbf015c146f25
Baptiste Leduc <baptiste@weglot.com>
2018-04-17 15:23:29 +0200

    implementing weglot/translate-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Weglot\TranslateBundle\WeglotTranslateBundle::class => ['all' => true],
 ];
